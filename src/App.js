7e780a1dd708d38ba83b207f979e5bc1e2f08950
Ian Santiago <ianfelipesantiago@gmail.com>
2022-07-13 00:22:00 -0300

    Limpando os padrões do App.js e criando Functions

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
+  let appStyle='App';
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save taao reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+  <div className={appStyle}>
+    <h1 style={ { color: 'gold' } }>Hello World</h1>
+    <p style={ {color: 'black'} }> Lorem ipsun </p>
+  </div>
+  <div>Goodbye</div>
+  </>
   );
 }
+
 
 export default App;
